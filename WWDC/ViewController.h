3f26ff088ab67e59d602425a3deac53e5e321b68
Aaron Stephenson <aaron@bronron.com>
2015-11-17 09:50:32 +1100

    Initial Commit

---
+++ WWDC/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WWDC
+//
+//  Created by Aaron Stephenson on 17/11/2015.
+//  Copyright © 2015 Bronron Apps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ WWDC/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  WWDC
+//
+//  Created by Aaron Stephenson on 17/11/2015.
+//  Copyright © 2015 Bronron Apps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
